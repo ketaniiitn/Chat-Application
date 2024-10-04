ad8b33e1570bdcac5cc4b5c634b30a86f062b5e6
ketaniiitn <ketanbajpai980@gmail.com>
2024-10-04 11:46:09 +0530

    All functionalities done

--- frontend/src/components/Sidebar/SearchInput.jsx
+++ frontend/src/components/Sidebar/SearchInput.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import {useState} from 'react';
 import { MdOutlinePersonSearch } from "react-icons/md";
+import useGetConversations from '../../hooks/useGetConversations';
+import useConversation from '../../zustand/useConversation';
+import toast from 'react-hot-toast';
 const SearchInput = () => {
+  const [search,setSearch] = useState("");
+  const {conversations}=useGetConversations();
+  const {setSelectedConversation}=useConversation();
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(search.length<3){
+      return toast.error('Search term must be at least 3 charachters long')
+    }
+    const conversation = conversations.find((c)=> c.fullName.toLowerCase().includes(search.toLowerCase()));
+    if(conversation){
+      setSelectedConversation(conversation)
+      setSearch('');
+    } else toast.error("No Such User Found");
+  }
   return (
-    <form className='flex items-center gap-2 px-4 pt-4 pb-0'>
+    <form onSubmit={handleSubmit} className='flex items-center gap-2 px-4 pt-4 pb-0'>
       <input 
         type="text" 
         placeholder="Search..." 
         className='input input-bordered rounded-full w-full mr-1' 
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}
       />
       <button 
         type='submit' 
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React, { useEffect,useRef } from 'react'
 import Message from './Message'
+import useGetMessage from '../../hooks/useGetMessage'
+import MessageSkeleton from '../skeletons/MessageSkeleton';
 const Messages = () => {
+  const {messages,loading}=useGetMessage();
+  const lastMessageRef =useRef();
+  useEffect(()=>{
+    setTimeout(()=>{
+      lastMessageRef.current?.scrollIntoView({behaviour:"smooth"});
+    })
+  },[messages])
   return (
     <div className='px-4 flex-1 overflow-auto'>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      
+      {!loading && messages.length>0 && messages.map((message)=> (<div key={message._id} ref ={lastMessageRef}><Message message={message}/></div>))}
+      {loading && [...Array(5)].map((_, idx) => <MessageSkeleton key={idx}/>)}
+      {!loading && messages.length === 0 && (<p className='text-center'>Send a message to start the conversation</p>)}
     </div>
   )
 }
