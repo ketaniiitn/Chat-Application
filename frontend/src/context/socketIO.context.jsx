fa2a1dc8e4a9980e270da955a8fd2b5862d7ebaf
ketaniiitn <149509716+ketaniiitn@users.noreply.github.com>
2024-10-04 17:27:03 +0530

    Update socketIO.context.jsx

--- frontend/src/context/socketIO.context.jsx
+++ frontend/src/context/socketIO.context.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("https://chat-mern-app-g219.onrender.com",{
+            const socket = io("https://localhost:8000",{
                query:{
                 userId:authUser._id,
 
