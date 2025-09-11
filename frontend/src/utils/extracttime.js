9431887d7e9791af8522f81cec692ea7ced1e6cd
KetanKites <ketan@thekites.ai>
2025-09-11 16:08:18 +0530

    CAPSLOCK issue

--- frontend/src/utils/extracttime.js
+++ frontend/src/utils/extracttime.js
@@ -1,9 +1,11 @@
-export function extracttime(dateString){
-    const date = new Date(dateString);
-    const hours = padZero(date.getHours());
-    const minutes = padZero(date.getMinutes());
-    return `${hours}:${minutes}`;
+export function extractTime(dateString) {
+	const date = new Date(dateString);
+	const hours = padZero(date.getHours());
+	const minutes = padZero(date.getMinutes());
+	return `${hours}:${minutes}`;
 }
-function padZero(number){
-    return number.toString().padStart(2,"0");
+
+// Helper function to ensure two digits
+function padZero(number) {
+	return number.toString().padStart(2, "0");
 }
